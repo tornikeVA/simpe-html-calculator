f679cf04d7d6033c8dfaf0e22ca30b097af90bbd
tornikeVA <vardiashvili00gmail.com>
2022-02-14 16:39:06 +0400

    Add listeners & change layout

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // author : julian pepe onzima
 
-var Calculator = function (containerId) {
+var Calculator = function (outputId, containerId) {
+    this.output = document.getElementById(outputId);
     this.container = document.getElementById(containerId);
     console.log(this.container);
     this.buttonNames = [
-        "0","1","2","3","4","5","6","7","8","9","0","+","-","*","/","=" 
+        "0","1","2","3","4","5","6","7","8","9",".","+","-","*","/","=" 
     ];
     this.createButtons(this.buttonNames);
 }
@@ -14,8 +15,12 @@ Calculator.prototype.createButtons = function (buttonNames) {
         let calculatorButton = document.createElement("button");
         calculatorButton.innerText = buttonNames[i];
         this.container.appendChild(calculatorButton);
+        let index = i;
+        calculatorButton.addEventListener("click", () => {
+            console.log(index);
+            this.output.innerHTML = buttonNames[index];
+        });
     }
 }
 
-var tCalculator = new Calculator("calculatorContainer");
-
+var tCalculator = new Calculator("output" , "calculatorContainer");
